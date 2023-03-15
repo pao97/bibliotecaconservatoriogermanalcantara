e0949fcf4917093e1214cde3a21c6ed7cd8a6a6d
Paola Pacheco <pachecopaola97@gmail.com>
2023-03-15 01:24:49 -0600

    Primer commit

---
+++ application/src/Job/IndexFulltextSearch.php
@@ -0,0 +1,42 @@
+<?php
+namespace Omeka\Job;
+
+use Omeka\Api\Adapter\FulltextSearchableInterface;
+use Omeka\Api\Adapter\ValueAnnotationAdapter;
+
+class IndexFulltextSearch extends AbstractJob
+{
+    /**
+     * Build the fulltext index for compatible resources.
+     */
+    public function perform()
+    {
+        $services = $this->getServiceLocator();
+        $api = $services->get('Omeka\ApiManager');
+        $em = $services->get('Omeka\EntityManager');
+        $fulltext = $services->get('Omeka\FulltextSearch');
+        $adapters = $services->get('Omeka\ApiAdapterManager');
+        foreach ($adapters->getRegisteredNames() as $adapterName) {
+            $adapter = $adapters->get($adapterName);
+            if ($adapter instanceof FulltextSearchableInterface
+                && !($adapter instanceof ValueAnnotationAdapter)) {
+                $page = 1;
+                do {
+                    if ($this->shouldStop()) {
+                        return;
+                    }
+                    $response = $api->search(
+                        $adapter->getResourceName(),
+                        ['page' => $page, 'per_page' => 100],
+                        ['responseContent' => 'resource']
+                    );
+                    foreach ($response->getContent() as $resource) {
+                        $fulltext->save($resource, $adapter);
+                    }
+                    $em->clear(); // avoid a memory leak
+                    $page++;
+                } while ($response->getContent());
+            }
+        }
+    }
+}
